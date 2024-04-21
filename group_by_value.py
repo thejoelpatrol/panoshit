892109d36dea5969a8b399703901e9a625c4b90c
Joel Cretan <jcretan@gmail.com>
2024-04-21 15:18:24 -0400

    use threads

--- group_by_value.py
+++ group_by_value.py
@@ -5,9 +5,12 @@ import subprocess
 import sys
 import traceback
 import json
+from concurrent.futures import ThreadPoolExecutor
+import time
 from typing import List, Dict
 
 EXTENSIONS = ["png", "jpg", "gif", "bmp", "tif", "tiff"]
+THREADS = 6
 
 class Continue(Exception):
     pass
@@ -29,38 +32,53 @@ def find_images(directory_path: str) -> List[str]:
     for f in os.listdir(directory_path):
         try:
             for e in EXTENSIONS:
-                if f.endswith(e):
+                if f.endswith('.' + e):
                     result.append(f)
                     raise Continue()
         except Continue:
             continue
     return result
 
+def process_image(path: str, cutoff: int, light: List[str], dark: List[str], light_path: str, dark_path: str):
+    result = run_magick(path)
+    lightness = result["channelStatistics"]["blue"]
+    if lightness["mean"] >= cutoff:
+        light.append(path)
+        shutil.copy(path, os.path.join(light_path, os.path.basename(path)))
+    else:
+        dark.append(path)
+        shutil.copy(path, os.path.join(dark_path, os.path.basename(path)))
+
 def main(args: argparse.Namespace):
     images = find_images(args.input_path)
     if len(images) == 0:
         raise FileNotFoundError(f"No files with images extensions found in {args.input_path}")
 
-    light = list()
-    dark = list()
+    light: List[str] = list()
+    dark : List[str] = list()
 
     light_path = os.path.join(args.input_path, f"light_{args.cutoff}")
     dark_path = os.path.join(args.input_path, f"dark_{args.cutoff}")
-    os.mkdir(light_path)
-    os.mkdir(dark_path)
+    try:
+        os.mkdir(light_path)
+        os.mkdir(dark_path)
+    except FileExistsError:
+        pass
 
     images.sort()
-    for f in images:
-        path = os.path.join(args.input_path, f)
-        print(path)
-        result = run_magick(path)
-        lightness = result["channelStatistics"]["blue"]
-        if lightness["mean"] >= args.cutoff:
-            light.append(path)
-            shutil.copy(path, os.path.join(light_path, f))
-        else:
-            dark.append(path)
-            shutil.copy(path, os.path.join(dark_path, f))
+    future_results = list()
+    with ThreadPoolExecutor(max_workers=THREADS) as thread_pool:
+        for f in images:
+            path = os.path.join(args.input_path, f)
+            future_results.append(thread_pool.submit(process_image, path, args.cutoff, light, dark, light_path, dark_path))
+    for i, r in enumerate(future_results):
+        while not r.done():
+            time.sleep(0.1)
+        
+        e = r.exception()
+        if (e):
+            print(traceback.format_exception(e))
+            sys.exit(1)
 
     print(f"light_{args.cutoff}: {len(light)}")
     print(f"dark_{args.cutoff}: {len(dark)}")
